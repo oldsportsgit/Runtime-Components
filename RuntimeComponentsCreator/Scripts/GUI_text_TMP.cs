dbb1073de80cef5f535daa831ffd8a8f706aceb7
a unity game developer <50415010+oldsportsgit@users.noreply.github.com>
2022-08-25 16:40:37 +0000

    quick edit for multiple gameobjects

--- RuntimeComponentsCreator/Scripts/GUI_text_TMP.cs
+++ RuntimeComponentsCreator/Scripts/GUI_text_TMP.cs
@@ -8,26 +8,27 @@ namespace RuntimeComponents.UI
  public class GUI_text_TMP : MonoBehaviour
  {
     // Start is called before the first frame update
-    public static void text_TMP(GameObject Canvas, Vector2 position, string text, Color colorOfText, int fontSize, TextAlignmentOptions align)
-    {
-          GUI_text = new GameObject("Text (TMP)", typeof(TextMeshProUGUI));
-          GUI_text.transform.SetParent(Canvas.transform);
-          GUI_text.GetComponent<TextMeshProUGUI>().text = text;
-          GUI_text.GetComponent<TextMeshProUGUI>().alignment = align;
-          GUI_text.GetComponent<TextMeshProUGUI>().color = colorOfText;
-          GUI_text.GetComponent<TextMeshProUGUI>().fontSize = fontSize;
-          GUI_text.GetComponent<RectTransform>().anchoredPosition = position;
+    public static void text_TMP(GameObject Canvas, Vector2 position, string text, Color colorOfText, int fontSize, TextAlignmentOptions align, int id)
+    {         
+          GameObject gobj = new GameObject("Text (TMP)", typeof(TextMeshProUGUI));
+          GUI_text.Add(gobj);
+          GUI_text[id].transform.SetParent(Canvas.transform);
+          GUI_text[id].GetComponent<TextMeshProUGUI>().text = text;
+          GUI_text[id].GetComponent<TextMeshProUGUI>().alignment = align;
+          GUI_text[id].GetComponent<TextMeshProUGUI>().color = colorOfText;
+          GUI_text[id].GetComponent<TextMeshProUGUI>().fontSize = fontSize;
+          GUI_text[id].GetComponent<RectTransform>().anchoredPosition = position;
     }
 
-    public static void Change(GameObject Canvas, Vector2 position, string text, Color colorOfText, int fontSize, TextAlignmentOptions align)
+    public static void Change(GameObject Canvas, Vector2 position, string text, Color colorOfText, int fontSize, TextAlignmentOptions align, int id)
     {
-          GUI_text.transform.SetParent(Canvas.transform);
-          GUI_text.GetComponent<TextMeshProUGUI>().text = text;
-          GUI_text.GetComponent<TextMeshProUGUI>().alignment = align;
-          GUI_text.GetComponent<TextMeshProUGUI>().color = colorOfText;
-          GUI_text.GetComponent<TextMeshProUGUI>().fontSize = fontSize;
-          GUI_text.GetComponent<RectTransform>().anchoredPosition = position;
+          GUI_text[id].transform.SetParent(Canvas.transform);
+          GUI_text[id].GetComponent<TextMeshProUGUI>().text = text;
+          GUI_text[id].GetComponent<TextMeshProUGUI>().alignment = align;
+          GUI_text[id].GetComponent<TextMeshProUGUI>().color = colorOfText;
+          GUI_text[id].GetComponent<TextMeshProUGUI>().fontSize = fontSize;
+          GUI_text[id].GetComponent<RectTransform>().anchoredPosition = position;
     }
-    private static GameObject GUI_text;
+    private static List<GameObject> GUI_text = new List<GameObject>();
  }
 }
