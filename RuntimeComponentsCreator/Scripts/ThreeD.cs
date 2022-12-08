c1e046e544340d1d85c388a05891e8e425480359
a unity game developer <50415010+oldsportsgit@users.noreply.github.com>
2022-12-08 17:02:56 +0100

    Update ThreeD.cs

--- RuntimeComponentsCreator/Scripts/ThreeD.cs
+++ RuntimeComponentsCreator/Scripts/ThreeD.cs
@@ -5,24 +5,23 @@ using TMPro;
 
 namespace RuntimeComponents
 {
- public class ThreeD
- {
-    public static GameObject TextTMPro(string name, Vector3 position, string text)
+    public class ThreeD
     {
-       GameObject threeD_text = new GameObject(name, typeof(TextMeshPro));
-       TextMeshPro threeDtextTMP = threeD_text.GetComponent<TextMeshPro>();
-       threeDtextTMP.text = text;
-       threeD_text.GetComponent<Transform>().position = position;
-       return threeD_text;
-    }
+        public static GameObject TextTMPro(string name, Vector3 position, string text)
+        {
+            GameObject threeD_text = new GameObject(name, typeof(TextMeshPro));
+            threeD_text.GetComponent<TextMeshPro>().text = text;
+            threeD_text.GetComponent<Transform>().position = position;
+            return threeD_text;
+        }
 
-    public static GameObject TextMesh(string name, Vector3 Position, string text)
-    {
-       GameObject textmesh = new GameObject(name, typeof(TextMesh));
-       textmesh.GetComponent<TextMesh>().text = text;
-       textmesh.GetComponent<MeshRenderer>().allowOcclusionWhenDynamic = false;
-       textmesh.GetComponent<Transform>().position = Position;
-       return textmesh;
+        public static GameObject TextMesh(string name, Vector3 Position, string text)
+        {
+            GameObject textmesh = new GameObject(name, typeof(TextMesh));
+            textmesh.GetComponent<TextMesh>().text = text;
+            textmesh.GetComponent<MeshRenderer>().allowOcclusionWhenDynamic = false;
+            textmesh.GetComponent<Transform>().position = Position;
+            return textmesh;
+        }
     }
- }
 }
