fb9ef8c0c21f17c52e711cd19055ebaff7708e9c
a unity game developer <50415010+oldsportsgit@users.noreply.github.com>
2022-12-16 22:44:13 +0100

    Update GameAssets.cs

--- RuntimeComponentsCreator/Scripts/GameAssets.cs
+++ RuntimeComponentsCreator/Scripts/GameAssets.cs
@@ -35,16 +35,28 @@ namespace RuntimeComponents.AssetBundle
             return cprefabs.ToArray();
         }
 
-        public static void CreateScene(string SceneName)
+        public static void CreateScene(string SceneName, bool IsBlankScene)
         {
             Scene newScene = SceneManager.CreateScene(SceneName);
             SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
             SceneManager.SetActiveScene(SceneManager.GetSceneByName(SceneName));
-            GameObject MainCamera = new GameObject("Main Camera", typeof(Camera), typeof(AudioListener));
-            GameObject DirectionalLight = new GameObject("Directional Light", typeof(Light));
-            RenderSettings.sun = DirectionalLight.GetComponent<Light>();
-            DirectionalLight.GetComponent<Light>().type = LightType.Directional;
-            DirectionalLight.GetComponent<Transform>().localEulerAngles = new Vector3(50, -30, 0);
+            if (!IsBlankScene)
+            {
+                GameObject MainCamera = new GameObject("Main Camera", typeof(Camera), typeof(AudioListener));
+                GameObject DirectionalLight = new GameObject("Directional Light", typeof(Light));
+                RenderSettings.sun = DirectionalLight.GetComponent<Light>();
+                DirectionalLight.GetComponent<Light>().type = LightType.Directional;
+                DirectionalLight.GetComponent<Transform>().localEulerAngles = new Vector3(50, -30, 0);
+            }
+        }
+
+        public static void CreateScene(string SceneName,bool IsBlankScene, params GameObject[] GameObjectsToAdd)
+        {
+            CreateScene(SceneName, IsBlankScene);
+            for (int i = 0; i < GameObjectsToAdd.Length; i++)
+            {
+                UnityEngine.Object.Instantiate(GameObjectsToAdd[i]);
+            }
         }
     }
 }
